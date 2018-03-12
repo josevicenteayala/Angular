cdaa0b68557bf2f0be54a938a756db52d177aa4a
DESKTOP-4LLK40V\Asus <jayala@intergrupo.com>
2018-03-12 16:03:32 -0500

    Another application

---
+++ SPA/src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {HeroesService, Heroe} from '../../services/heroes.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+  constructor(private _heroesService:HeroesService, private _router:Router ) {
+
+  }
+  heroes:Heroe[] = [];
+
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeroe(indice:number){
+    this._router.navigate(['/heroe',indice]);
+  }
+
+}
